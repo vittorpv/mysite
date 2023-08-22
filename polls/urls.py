93c3ba2dc6a9d87314e0a3fce2abad9babeee553
João Vitor Paiva <joaov.paiva777@outlook.com>
2023-08-22 15:11:27 -0300

    configure url for ResultsView

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('question/<int:pk>/', QuestionDetail.as_view(), name='question_detail')
+    path('question/<int:pk>/', QuestionDetail.as_view(), name='question_detail'),
+    path('question/<int:pk>/results/', ResultsView.as_view(), name='results'),
 ]
