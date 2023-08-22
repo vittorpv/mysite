939bb66a0942a75353ce5928097c619d865e4d1d
João Vitor Paiva <joaov.paiva777@outlook.com>
2023-08-22 16:16:14 -0300

    add feature votar

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('question/<int:pk>/', QuestionDetail.as_view(), name='question_detail'),
+    path("question/<int:pk>/vote/", vote, name="vote"),
     path('question/<int:pk>/results/', ResultsView.as_view(), name='results'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from .models import *
 
@@ -9,9 +12,26 @@ class IndexView(ListView):
 
 class QuestionDetail(DetailView):
     template_name = 'question.html'
-    model = Question
+    model = Question    
     
 class ResultsView(DetailView):
     template_name = 'results.html'
     model = Question 
     
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "question.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("results", args=(question.id,)))
