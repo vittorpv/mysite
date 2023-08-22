ff111fc6033651c2bf8bba4ec147c044ab0dc70d
João Vitor Paiva <joaov.paiva777@outlook.com>
2023-08-22 14:54:09 -0300

    add votes Field on model Choice

--- polls/models.py
+++ polls/models.py
@@ -7,3 +7,4 @@ class Question(models.Model):
 class Choice(models.Model):
     choice_text = models.CharField(max_length=150)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    votes = models.PositiveSmallIntegerField()
